28460df10ac2174b413a7fc7ded1da807518adb0
Jaehong Lee <ja34luv@gmail.com>
2025-07-07 23:13:07 -0700

    feat: Update background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import Card from "components/Card";
 
 export function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-white">
       <Head title="WireLess" />
 
       <Navbar
@@ -14,25 +14,29 @@ export function Home() {
         selectedMode={NAVBAR_ENUM.HOME}
       />
 
-      <div className="flex flex-col items-center justify-center flex-1">
-        <DefaultHeading
-          title={HOME_PAGE.TITLE}
-          description={HOME_PAGE.DESCRIPTION}
-        />
+      <div className="flex-1 bg-white bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] relative">
+        <div className="bg-gradient-to-b from-blue-50 to-transparent absolute top-0 left-0 w-full h-full z-0" />
 
-        <div className="h-10" />
+        <div className="flex flex-col items-center justify-center absolute top-0 left-0 w-full h-full z-20">
+          <DefaultHeading
+            title={HOME_PAGE.TITLE}
+            description={HOME_PAGE.DESCRIPTION}
+          />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-4xl w-full px-4">
-          {HOME_PAGE.CARDS.map((card) => (
-            <Card
-              key={card.TITLE}
-              emoji={card.EMOJI}
-              title={card.TITLE}
-              description={card.DESCRIPTION}
-              buttonText={card.BUTTON_TEXT}
-              disabled={card.DISABLED}
-            />
-          ))}
+          <div className="h-10" />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-4xl w-full px-4">
+            {HOME_PAGE.CARDS.map((card) => (
+              <Card
+                key={card.TITLE}
+                emoji={card.EMOJI}
+                title={card.TITLE}
+                description={card.DESCRIPTION}
+                buttonText={card.BUTTON_TEXT}
+                disabled={card.DISABLED}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
